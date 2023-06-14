82db2c914b1f7120a2068dd3cd5aa7450cd8b9ad
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-06-14 10:46:50 +0600

    Extra section About us done

---
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -0,0 +1,32 @@
+import { Helmet } from "react-helmet-async";
+import about_img from "../../../assets/about_us.jpg"
+import { Link } from "react-router-dom";
+
+
+const AboutUs = () => {
+    return (
+        <div className="mb-12">
+            <Helmet>
+                <title> MUZ SPORTS | About Us</title>
+            </Helmet>
+
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={about_img} className="max-w-sm rounded-lg shadow-2xl h-[400px]" />
+                    <div className="w-1/2">
+                        <h1 className="text-5xl font-bold">About Us</h1>
+                        <p className="py-6">Welcome to MUZ SPORTS - Your Ultimate Destination for Sporting Excellence!
+
+                            At MUZ SPORTS, we are passionate about sports and dedicated to providing you with a comprehensive platform that caters to all your sporting needs. Whether you are an avid athlete, a passionate fan, or someone looking to explore the world of sports, our website is designed to offer an immersive and informative experience.
+
+                            As you explore our website, you will find a wide range of features and resources that make us a one-stop hub for all things sports. Let us take you on a journey through the key highlights of MUZ SPORTS:</p>
+                      <Link to = '/classes'>  <button className="btn btn-primary">LEARN MORE</button></Link>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -23,7 +23,7 @@ const Banner = () => {
                     <div className='p-10 md:p-28 lg:p-28 md:w-1/2 mt-12'>
                         <h1 className='text-2xl md:text-xl lg:text-5xl text-white font-semibold mt-10'> <span className='text-green-500'>Yes to sports,</span> <span className='text-red-500'>no to drugs</span> </h1>
                         <p className='text-white mt-5'> “Somebody gives you an opportunity, say yes to it. So what if you fail? you won’t know if you fail or succeed unless you try.” <span className='text-red-400'> —Ann Meyers</span></p>
-                       <Link> <button className='btn btn-secondary mt-3 border-none text-white'> View Classes</button></Link>
+                       <Link to = '/classes'> <button className='btn btn-secondary mt-3 border-none text-white'> View Classes</button></Link>
                     </div>
                 </div>
 
